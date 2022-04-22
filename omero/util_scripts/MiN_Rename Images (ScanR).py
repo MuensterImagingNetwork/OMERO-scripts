d9487487116330533784e52020a27bc5aa49603b
Münster Imaging Network <imaging@uni-muenster.de>
2022-04-22 17:28:27 +0200

    updated for more usecases (upload without renaming metadata file) ; adapted to code changes (generation of name list)

--- omero/util_scripts/MiN_Rename Images (ScanR).py
+++ omero/util_scripts/MiN_Rename Images (ScanR).py
@@ -26,21 +26,22 @@ def rename_images(conn, script_params):
 
         #   get the names of the original images out of the list of imported file names
         pathList = imageList[0].getImportedImageFilePaths()["server_paths"]
-        origNameList = [[name.split("/")][-1] for name in pathList]
+        origNameList = [[name.split("/")][-1][-1] for name in pathList]
         #   remove the metadata file
         for x in origNameList:
-            if x.startswith("metadata"):
+            if str(x).startswith("metadata"):
                 origNameList.remove(x)
 
         #   looks like this: wendtj_2611/2022-01/18/14-29-55.807/P9--W00369--P00002--Z00000--T00000--mcherry.tif
         #   format them to remove channel, .tif and the path prefix
         #   into --> P9--W00369--P00002--Z00000--T00000
+        newNameList=[]
         for name in origNameList:
             last_index = [(m.start(0)) for m in re.finditer("--", name)][-1]  # finds last index of "--" in the name
-            name = name[:last_index]
+            newNameList.append(name[:last_index])
 
         #   convert to a set to avoid duplicates
-        origPathSet = set(origNameList)
+        origPathSet = set(newNameList)
 
         if len(origPathSet)>0:
             print("Succesfully created set of original names")
